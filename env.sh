26de6f3d85f7b7424591770b003ff3194ccef3af
Nicaury Benitez <nicaurybenitez@dorna.com>
2020-05-06 03:20:13 +0200

    add env files

---
+++ env.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+#idioma FB: es, en, it, fr, de, bpr, ppr
+export LANG=es
+#Ruta de Descarga de Imágenes
+export IMG_PATH=/home/splinter/images
+#Ancho de la pantalla (Resolucion)
+export SIZE_X=1366
+#Alto de la pantalla (Resolucion)
+export SIZE_X=768
+#User Agent
+#export USER_AGENT= "Mozilla/5.0 (Windows NT 6.1; rv:64.0) Gecko/20100101 Firefox/64.0"
+#Cuenta FB
+export USER=elmetacrilatos@gmail.com
+#Password FB
+export PASSWORD=xxxxxxxxxxxxxxx
+#Tiempo Minimo de Espera Entre Acciones
+export MIN_WAIT_TIME=30
+#Tiempo Má de Espera Entre Acciones
+export MAX_WAIT_TIME=60
+#Correr en Modo Headless = 1-> Si, 0-> No
+export HEADLESS=0
+#Guardar ScreenShots = 1-> Si, 0-> No
+export SAVE_SS=0
+#Categoria de Items En FB: /vehicles/,/electronics/,/home/,/hobbies/,/propertyforsale/,/classifieds/,/apparel/,/propertyrentals/,/entertainment/,/family/
+export CATEGORY=/electronics/
+#Ubicación Geografica De Búsqueda (Vacío usa la predeterminada)
+export LOCATION=Miami
+#Cantidad de veces a hacer scroll en el listado de items (para cargar más)
+export SCROLL_TIMES=3
+#Parametros MYSQL
+export MYSQL_HOST=localhost
+export MYSQL_PORT=3307
+export MYSQL_DB=ezzyads-devs
+export MYSQL_USER=admin
+export MYSQL_PASSWORD=z9mS48lDyScNanW2EO5A
+export SCROLL_TIMES=3
+export USER_P=hello@ezzyads.com
+export PASSWORD_P=XXXXXXXXXXXXXX
